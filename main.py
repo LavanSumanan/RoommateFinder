bc3a5a1823f26a68ef65ea251043685c29a7bb93
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-05-22 17:28:18 -0400

    added firestore db

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from flask import Flask, requests, direct, url_for
+from flask import Flask, request, redirect, url_for, jsonify
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore, initialize_app
 
+# initialize firestore db
+cred = credentials.Certificate('firestorekey.json')
+default_app = initialize_app(cred)
+# the actial db
+db = firestore.client()
+
+# flask app
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
-    else:
-        return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>Hey, {usr}</h1>"
+@app.route("/form", methods=["POST", "GET"])
+def form():
+    if request.method == "POST":
+        print(request.form.to_dict())
+        user_name = request.form["nm"]
+        user_age = request.form["age"]
+        doc_ref = db.collection(u'test').document(u'firstuser')
+        doc_ref.set({
+            u'name': user_name,
+            u'age': user_age
+        })
+        # method to put data into database
+    else:
+          return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
