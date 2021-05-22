230b6d5153b545e0edd2dbb696bac45f1b9c89a6
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-05-22 18:00:01 -0400

    Created results page to display data stored in firebase db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, redirect, url_for, jsonify
+from flask import Flask, request, redirect, url_for, jsonify, render_template
 import os
+
 import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 
@@ -9,6 +10,10 @@ default_app = initialize_app(cred)
 # the actial db
 db = firestore.client()
 
+# method to retrieve matches given a user id
+# def retrieve_matches(user_id):
+    # TODO
+
 # flask app
 app = Flask(__name__)
 
@@ -16,18 +21,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/result/<user_id>")
+def result(user_id):
+    doc_ref = db.collection(u'test').document(user_id)
+    doc = doc_ref.get()
+    age_of_user = doc.to_dict()["age"]
+    return render_template("result.html", age=age_of_user)
 
 @app.route("/form", methods=["POST", "GET"])
 def form():
     if request.method == "POST":
-        print(request.form.to_dict())
-        user_name = request.form["nm"]
+        user_id = request.form["nm"]
+        user_name = user_id
         user_age = request.form["age"]
-        doc_ref = db.collection(u'test').document(u'firstuser')
+        doc_ref = db.collection(u'test').document(user_id)
         doc_ref.set({
             u'name': user_name,
             u'age': user_age
         })
+        return redirect(url_for("result", user_id=user_id))
         # method to put data into database
     else:
           return render_template("form.html")
