756e6ed8a2e223d9a80871d6fd6622416a34e4ee
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-05-22 14:27:34 -0400

    Added pages and requests

--- main.py
+++ main.py
@@ -2,5 +2,21 @@ from flask import Flask, requests, direct, url_for
 
 app = Flask(__name__)
 
-if __name__ == "__main__"
-app.run(debug=True)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>Hey, {usr}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
