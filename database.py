0a30851d7bb8f15060e3fd6e7ba04808c467345d
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-05-24 14:41:36 -0400

    Worked on querying

--- database.py
+++ database.py
@@ -12,18 +12,14 @@ def retrieve_matches(user_id):
     doc_ref = db.collection(u'users').document(user_id)
     doc = doc_ref.get()
     user_data = doc.to_dict()
-    query = db.collection(u'users')
-    # query = query.where(u'id', u'!=', user_id)
-    # if data["roomgender"] == "No":
-    #     query = query.where(u'gender', u'==', data["gender"])
-    # query = query.where(u'lengthofstay', u'==', data["lengthofstay"])
-    # query = query.where(u'university', u'==', data["university"])
-    query = query.limit(2)
+    query = db.collection(u'users').where(u'id', u'!=', user_id)
+    # if user_data["roomgender"] == "No":
+    #     query = query.where(u'gender', u'==', user_data["gender"])
+    # query = query.where(u'lengthofstay', u'==', user_data["lengthofstay"])
+    # query = query.where(u'university', u'==', user_data["university"])
+    query = query.limit(3)
     docs = query.stream()
     matches = []
     for doc in docs:
-        data = doc.to_dict()
-        if not data["id"] == user_id and data["university"] == user_data["university"]:
-            matches.append(doc.to_dict())
+        matches.append(doc.to_dict())
     doc_ref.update({"matches": matches})
-    
